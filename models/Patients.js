7e22b9a1c2c4adebd0250338c236db4f81a64e24
mnz02 <abdulminhaz47@gmail.com>
2024-08-08 00:32:29 +0530

    feat: Remove patientId field from Patients model
    The patientId field has been removed from the Patients model as it is no longer needed. This change simplifies the model and reduces unnecessary data storage.

--- models/Patients.js
+++ models/Patients.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose'
 
 const patientsSchema = new mongoose.Schema({
-  patientId: {
-    type: String,
-    required: true,
-    unique: true
-  },
   name: {
     type: String,
     required: true
