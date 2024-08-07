ee87151acd240ce73ea777b545cc57d7c2f1ee80
mnz02 <abdulminhaz47@gmail.com>
2024-08-08 01:21:46 +0530

    feat: Add unique patientId field to Patients model
    The patientId field has been added to the Patients model to uniquely identify each patient. This allows for easier retrieval and management of patient records.

--- models/Patients.js
+++ models/Patients.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
 
 const patientsSchema = new mongoose.Schema({
+  patientId: {
+    type: String,
+    unique: true
+  },
   name: {
     type: String,
     required: true
